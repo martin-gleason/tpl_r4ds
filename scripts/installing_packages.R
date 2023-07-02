be4e750a3b8cd7ed55511825544ebd811de88151
Martin Gleason <marty.data.scientist@icloud.com>
2023-07-02 13:42:13 -0500

    inital commit

---
+++ scripts/installing_packages.R
@@ -0,0 +1,11 @@
+# Installer Script
+# All the packages we will need for our R4DS Experience™
+
+
+install.packages("tidyverse", "here", "googlesheets4")
+install.packages("palmerpenguins")
+
+library(palmerpenguins)
+
+x <- "Hello World"
+
