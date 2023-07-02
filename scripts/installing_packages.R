9b48e2db118432662e075bc71a1c60068ab634db
Martin Gleason <marty.data.scientist@icloud.com>
2023-07-02 13:52:38 -0500

    add readme

--- scripts/installing_packages.R
+++ scripts/installing_packages.R
@@ -5,7 +5,11 @@
 install.packages("tidyverse", "here", "googlesheets4")
 install.packages("palmerpenguins")
 
+library(here)
+
 library(palmerpenguins)
 
 x <- "Hello World"
 
+penguins %>%
+  write_csv("inputs/penguins.csv")
