29d068de661ebded945c8cac0644b14a46b8475e
Martin Gleason <marty.data.scientist@icloud.com>
2023-07-30 10:02:18 -0500

    pushing a polar plot

--- scripts/README.R
+++ scripts/README.R
@@ -7,7 +7,13 @@ penguins <- read_csv("inputs/penguins.csv")
 
 glimpse(penguins)
 
-penguins %>%
+data_viz <- penguins %>%
   ggplot(aes(x = year, y = bill_length_mm, fill = species)) +
   geom_bar(stat = "identity", position = "dodge") +
-  facet_grid(~sex)
+  facet_grid(~sex) + 
+  theme_minimal() +
+  labs(x = "Year", y = "Bill Length (Milimeters)", 
+       fill = "Species", 
+       title = "Palmer Penguins: 2007 - 2009")
+
+ggsave("outputs/penguins.png", data_viz)
--- scripts/installing_packages.R
+++ scripts/installing_packages.R
@@ -3,13 +3,60 @@
 
 
 install.packages("tidyverse", "here", "googlesheets4")
+install.packages("janitor")
 install.packages("palmerpenguins")
 
 library(here)
-
+library(tidyverse)
 library(palmerpenguins)
 
 x <- "Hello World"
 
 penguins %>%
   write_csv("inputs/penguins.csv")
+
+penguins_raw
+
+nrow(penguins)
+nrow(penguins_raw)
+
+
+
+names(penguins)
+
+#datafame
+
+x <- "A"
+
+TRUE
+FALSE
+
+penguins$species
+
+View(penguins)
+
+#this is a comment
+ggplot(data = penguins, aes(x = bill_length_mm, y = flipper_length_mm)) + 
+  geom_point(aes(color = species)) +
+  geom_smooth() +
+  facet_wrap(~sex) +
+  labs(title = "Some penguin shit",
+       x = "Bill Size",
+       y = "Goddamn Flippers!",
+       color = "What Kinda Penguin") +
+  theme_minimal()
+
+
+penguins %>%
+  filter(species == "Gentoo") %>%
+  ggplot(aes(x = bill_length_mm, y = flipper_length_mm)) + 
+  geom_smooth() +
+  facet_wrap(~sex) +
+  labs(title = "Some penguin shit",
+       x = "Bill Size",
+       y = "Goddamn Flippers!",
+       color = "What Kinda Penguin") +
+  theme_minimal()
+
+"AND THEN"
+
